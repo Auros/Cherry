89ea19ac04bdafa3e5e5a9cd128e7bb0bbb813ce
Auros Nexus <aurosnex@gmail.com>
2021-02-14 22:39:54 -0500

    Initial commit

---
+++ Cherry/Plugin.cs
@@ -0,0 +1,29 @@
+﻿using IPA;
+using SiraUtil;
+using SiraUtil.Zenject;
+using IPALogger = IPA.Logging.Logger;
+
+namespace Cherry
+{
+    [Plugin(RuntimeOptions.DynamicInit)]
+    public class Plugin
+    {
+        [Init]
+        public Plugin(IPALogger logger, Zenjector zenjector)
+        {
+            zenjector.On<PCAppInit>().Pseudo(Container => Container.BindLoggerAsSiraLogger(logger));
+        }
+
+        [OnEnable]
+        public void OnEnable()
+        {
+
+        }
+
+        [OnDisable]
+        public void OnDisable()
+        {
+
+        }
+    }
+}
